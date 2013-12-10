14ee4f28f31be17a9b2a0a252733c389276f694e
Larry Fortna <Larry@Larrys-Mac-mini-350.local>
2013-12-10 18:23:33 -0500

    Initial Commit

---
+++ DiceRollApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DiceRollApp
+//
+//  Created by Larry Fortna on 12/10/13.
+//  Copyright (c) 2013 Larry Fortna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DiceRollApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DiceRollApp
+//
+//  Created by Larry Fortna on 12/10/13.
+//  Copyright (c) 2013 Larry Fortna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
